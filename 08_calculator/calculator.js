e59bec5a934d53889ad4b75a668b66be20acb315
hoomanhorsey <94696321+hoomanhorsey@users.noreply.github.com>
2023-10-31 02:30:54 +1100

    Just finished exercise 10

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (a - b);
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const sum = function(arr) {
+	  return arr.reduce((sum, current) => sum + current, 0);
 
 };
 
-const power = function() {
-	
+const multiply = function(arr) {
+    return arr.reduce((sum, current) => sum * current);
+
 };
 
-const factorial = function() {
-	
+const power = function(a, b) {
+  let sum = a;
+  for (let i = 1; i < b; i++) {
+      sum *= a;
+  };
+  return sum;
+};
+
+const factorial = function(a) {
+	if (a === 0) {
+    return 1;
+  }
+  let factorial = a;
+  let multiplier = a - 1;
+  for (let i = a; i > 1; i--) {
+    factorial = factorial * multiplier;
+    multiplier = multiplier - 1;
+  }
+  return factorial; 
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (input) {
 
+const rawString = input.split("");
+
+// clean string of punctuation and spaces
+const string = [];  // array to store string stripped of non-alpha + whitespace
+
+for (let i = 0; i < rawString.length; i++){
+    if (testAlphaNum(rawString[i])) {
+        string.push(rawString[i]);
+        }}
+
+// get string length
+let stringLen = string.length;
+let halfStringLen = Math.floor(stringLen / 2);
+
+for (let i = 0; i < halfStringLen; i++)
+    if (string[i].toLowerCase() != string[stringLen-1].toLowerCase()) {   
+        return false;
+    } else { 
+        stringLen -= 1; }
+    return true;
 };
+
+function testAlphaNum(char) {
+    return /^[a-zA-Z0-9]$/.test(char);}
 
 // Do not edit below this line
 module.exports = palindromes;
