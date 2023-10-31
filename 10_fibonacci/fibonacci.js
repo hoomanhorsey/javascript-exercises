8e67bda5c0ecd78705212affd6d0fced84bda8a1
hoomanhorsey <94696321+hoomanhorsey@users.noreply.github.com>
2023-10-31 16:45:20 +1100

    Final js exercises completed, my solutions are sometimes more convoluted or more concise than the official solutions. Not sure what to make of that!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,39 +1,25 @@
-const fibonacci = function("5") {
-    if (count < 0) return "OOPS";
-    if (count === 0) return 0;
 
-    let firstPrev = 1;
-    let secondPrev = 0;
-    
-    for (let i = 2; i <= count; i++) {
-        let current = firstPrev + secondPrev;
-        secondPrev = firstPrev;
-        firstPrev = current;
-    }
 
-    return firstPrev;
-};
+const fibonacci = function(rawInput) {
 
-// const fibonacci = function(rawInput) {
+let input = parseInt(rawInput);
 
-// let input = parseInt(rawInput);
+if (input < 0) return 'OOPS';
 
-// if (input < 0) return 'OOPS';
+let n = input;
+const sequence = [1, 1,];
 
-// let n = input;
-// const sequence = [1, 1,];
+let tally = 0;
+for (let i = 0; i < n; i++) {
+    tally = sequence[n - (n - i)] + sequence[n - (n - (i + 1))];
+    sequence.push(tally); 
+    };
 
-// let tally = 0;
-// for (let i = 0; i < n; i++) {
-//     tally = sequence[n - (n - i)] + sequence[n - (n - (i + 1))];
-//     sequence.push(tally); 
-//     };
-
-// return sequence[input-1];
+return sequence[input-1];
    
      
 
-// };
+ };
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+
+
+const oldest = arr.sort(function(a, b) {
+    if (a.yearOfDeath == null) {
+        date = new Date();
+        a.yearOfDeath = date.getFullYear();
+    }
+    if (b.yearOfDeath 
+        == null) {
+        date = new Date();
+        b.yearOfDeath = date.getFullYear();
+    }
+   
+    aPerson = a.yearOfDeath - a.yearOfBirth;
+    bPerson = b.yearOfDeath - b.yearOfBirth;
+    return aPerson > bPerson ?  -1 : 1; 
+});
+
+return oldest[0];
+
+
 
 };
 
